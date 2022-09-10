7b6413d98e6be00699d41c86d84dcdf2205fac87
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-09-10 15:30:58 +0530

    feat: background change with color Scheme
    Now, Background will change with every color Scheme you choose. It will change into a gradient of first and last color of the scheme.

--- script.js
+++ script.js
@@ -6,20 +6,28 @@ const colorSelect = document.getElementById("seed-color");
 const getSchemeBtn = document.getElementById("color-btn");
 const colors = document.querySelectorAll(".color");
 const hexCodes = document.querySelectorAll(".hex");
+const bodyEl = document.querySelector("body");
 
 function getColorSchemes() {
     const mode = selectMode.value;
     const seedColor = colorSelect.value;
 
-    // TO remove the hash symbol for hex code
-    const regex = /^#/;
-    const hexCode = seedColor.replace(regex,"");
+    // To remove the hash symbol for hex code 
+    // reason: b/c API needs only hex code without hash Symbol
+    const hexCode = seedColor.replace("#","");
 
-    // Fetching the color Scheme
+    // Fetching the color Scheme from the API
     fetch(`https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${mode}&count=5`)
         .then(response => response.json())
         .then(data => {
             const colorArr = data.colors;
+        
+            // Change body element background with gradient of first and last color
+            const lastColor = colorArr[colorArr.length - 1].hex.value;
+            const firstColor = colorArr[0].hex.value;
+            bodyEl.style.backgroundImage = `linear-gradient(to right, ${firstColor} ,${lastColor}`;
+
+            // Getting color Scheme
             for(let i = 0; i < colorArr.length; i++) {
                 const hex = colorArr[i].hex.value;
                 colors[i].style.backgroundColor = hex;
